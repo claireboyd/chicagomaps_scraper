47a86deb44de11ad722994c828df0d5da5ee6b48
Claire Boyd <claire.k.boyd@gmail.com>
2023-01-25 20:30:51 +0000

    1.25

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 import lxml.html
 import requests
+import pandas as pd
 
-# helper function to streamline two lines of code used a few times
+# helper function to get the root HTML object
 def get_root(url):
     '''
     Returns root element given a URL.
@@ -9,91 +10,82 @@ def get_root(url):
     Returns: root (lxml.html.HtmlElement)
     '''
     req = requests.get(url)
-    # print(req.text)
     html = req.text
     root = lxml.html.fromstring(html)
 
     return root
 
-root = get_root("http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible_files/sheet001.htm")
-
+# core function
 def scrape_page(root):
     """
     This function takes a URL of the City of Chicago Maps and re-standardizes
-    the data into a workable format
+    the data into a workable format.
 
     Inputs: URL
     Returns: CSV file with parsed data
     """
-    #url = "http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible_files/sheet001.htm"
-    #root = get_root(url)
-
+    # initialize maps dict and row number
     maps = {}
+    row_number = 0
 
-    # all trs are under this
-    all_rows = root.cssselect('body table')[0][11:]
-    #print(len(all_rows))
+    # all trs are under this. #slicing after row 18 because the first 18 rows are header text for the page
+    all_rows = root.cssselect('body table')[0][18:]
+    
 
     for i, row in enumerate(all_rows):
-        row_number = i
+        #unpack all tds in row
         row_tds = row.getchildren()
 
-        if "mso-yfti-irow" in row.get("style"):
-            
-            #assignment based on order
-            map_dict = {}
+        # if there are 6 tds, it means that it is a full row of data. save content accordingly
+        if len(row_tds) == 6:
+
+            # increment row number when we hit a new full row of data
+            row_number += 1
+
+            # create key with row_number and value with a dictionary for that row, starting with No.
             maps[str(row_number)] = {"No.": row_tds[0].text_content()}
 
-            # for element in row_tds:
-            #     maps["No."] = element.text_content() 
-            # # row_idx = i
-            # # print(row.cssselect('td')[0])
-            # break
-
-            # print(row.cssselect('td')[0])
-            # maps["No."].get(row.cssselect('tr')[0], None)
-        else:
+            # add all columns to that row dictionary
             maps[str(row_number)]["Title"] = row_tds[1].text_content()
+            maps[str(row_number)]["Author"] = row_tds[2].text_content()
+            maps[str(row_number)]["Size"] = row_tds[3].text_content()
+            maps[str(row_number)]["Scale"] = ""
+            maps[str(row_number)]["Description"] = ""
+            maps[str(row_number)]["Provenance"] = ""
+            maps[str(row_number)]["Year"] = row_tds[4].text_content()
+            maps[str(row_number)]["Category"] = row_tds[5].text_content()
+
+        elif len(row_tds) == 1:
+            text = row_tds[0].text_content().strip().replace("\n", "")
+
+            if "Gift" in text:
+                maps[str(row_number)]["Provenance"] = text
+            elif "Scale" in text:
+                maps[str(row_number)]["Scale"] = text
+            elif "\"x" in text:
+                maps[str(row_number)]["Size"] += text
+            else:
+                maps[str(row_number)]["Description"] += text + " "
+
+    # string parsing for size columns
+    for row_dict in maps.values():
+        
+
 
 
     return maps
 
-    # column_titles = all_rows[0]
+def get_csv(root):
+    """
+    """
+    #root = get_root("http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible_files/sheet001.htm")
+    maps_dict = scrape_page(root)
 
-    # for col in column_titles:
-    #     print(col.text_content())
+    #convert to dataframe
+    maps_df = pd.DataFrame.from_dict(maps_dict, orient='index')
 
-    # for tr in all_rows[0:5]:
-    #     if 
-    #     print(tr.style.text_content())
+    #return maps_df
 
-
-
-
-
-    # for row in all_rows:
-    #     print(row.text_content())
-
-    # return all_rows
-
-
-    # park_dict["address"] = root.cssselect('p.address')[0].text_content().strip()
-    # park_dict["url"] = url
-
-    # # getting root of block text to loop through view-content sections
-    # block_text = root.cssselect('div.section')[0].cssselect('div.view-content')
-
-    # for block in block_text:
-    #     # read description to dict
-    #     if block.cssselect('h3.block-title')[0].text_content().strip() == "Description":
-    #         park_dict["description"] = block.cssselect('div.block-text')[0].text_content()
-
-    #     # read history to dict
-    #     if block.cssselect('h3.block-title')[0].text_content().strip() == "History":
-    #         park_dict["history"] = block.cssselect('div.block-text')[0].text_content()
-
-    # #create key/value pair of history: "" if no history block on webpage
-    # park_dict["history"] = park_dict.get("history", "")
-
-    # return park_dict
-
+    # save to csv in filepath folder
+    maps_df.to_excel("output/city_of_chicago_maps.xlsx",
+                     sheet_name='City of Chicago Maps')
