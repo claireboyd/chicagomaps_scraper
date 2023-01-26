1664cc7f60c2ae52fb37e08aa77b4b4b82ba900b
Claire Boyd <claire.k.boyd@gmail.com>
2023-01-26 21:39:21 +0000

    fixed problem with scale, commented all code, renamed functions

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Scraper for Chicago Maps Archive
-Scraping data from chicago maps inventory for better digitization processing.
+
+The purpose of this scraper is to reconfigure data made publicly available by the Chicago History Museum on [City of Chicago Maps](http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible.htm). This reconfigured data will be used to help process and digitize the maps for public use.
+
+
+
+
+In order to streamline the use of this scraper, I used the python package [poetry](https://python-poetry.org/docs/) to create a virtual environment. This package allows users to 
+
+This repository has the following components:
+
+* **scraper.py**: 
+
+* **scraper.py**: 
+
+
+
+## A note on the structure of the webpage:
+
+The main content is all built into one table body, with each tr being a new "line" of the webpage content. The core reason why pulling this content is challenging is because each row of the table has multiple trs, so treating that as one text block (especially when the number of lines in each row changes) required additional construction.
--- scraper.py
+++ scraper.py
@@ -1,115 +1,131 @@
+# packages for reading in the html
 import lxml.html
 import requests
+
+# packages for string manipulation and converting into a dataframe
+import regex as re
 import pandas as pd
 
-# helper function to get the root HTML object
-def get_root(url):
-    '''
-    Returns root element given a URL.
-    Inputs: None
-    Returns: root (lxml.html.HtmlElement)
-    '''
-    req = requests.get(url)
-    html = req.text
-    root = lxml.html.fromstring(html)
-
-    return root
-
-# core function
-def scrape_page(root):
+def page_to_dict(url):
     """
-    This function takes a URL of the City of Chicago Maps and re-standardizes
-    the data into a workable format.
+    This function takes the URL and restructures the data into a 
+    dictionary of dictionaries, parsing by types of text.
 
     Inputs: URL
-    Returns: CSV file with parsed data
+    Returns: maps (dict of dicts), with the key as a row number and 
+    the value as another dictionary with all cleaned row data.
     """
-    # initialize maps dict and row number
+    # get root HTML object or webpage
+    html = requests.get(url).text
+    root = lxml.html.fromstring(html)
+    
+    # initialize a dictionary and set row number (or future dictionary key) to 0
     maps = {}
     row_number = 0
 
-    # all trs are under this. #slicing all trs after row 18 because the first 18 rows are header text for the webpage
+    # collect all table rows (trs) on the webpage, slicing after row 18 where the content of the table begins
     all_rows = root.cssselect('body table')[0][18:]
 
-    for i, row in enumerate(all_rows):
-        #unpack all tds in row
+    # iterate through each row of data to see where/how to save the data to each row's dictionary
+    for row in all_rows:
+        #unpack all table data points (tds) in the row
         row_tds = row.getchildren()
 
-        # if there are 6 tds, it means that it is a full row of data. save content accordingly
+        # if there are 6 tds in the row, it means that it is a full row of data. save content accordingly.
         if len(row_tds) == 6:
-
             # increment row number when we hit a new full row of data
             row_number += 1
 
-            # create key with row_number and value with a dictionary for that row, starting with No.
-            maps[str(row_number)] = {"No.": row_tds[0].text_content()}
+            # add key/value pair in dictionary, with row_number (integer): {No.: [value]} (dictionary)
+            maps[row_number] = {"No.": row_tds[0].text_content()}
 
-            # add all columns to that row dictionary
-            maps[str(row_number)]["Title"] = row_tds[1].text_content()
-            maps[str(row_number)]["Author"] = row_tds[2].text_content()
-            maps[str(row_number)]["Size"] = row_tds[3].text_content()
-            maps[str(row_number)]["Width (in)"] = ""
-            maps[str(row_number)]["Height (in)"] = ""
-            maps[str(row_number)]["Scale"] = ""
-            maps[str(row_number)]["Description"] = ""
-            maps[str(row_number)]["Provenance"] = ""
-            maps[str(row_number)]["Year"] = row_tds[4].text_content()
-            maps[str(row_number)]["Category"] = row_tds[5].text_content()
+            # add the rest of the tds as content to dictionary, with relevant keys
+            maps[row_number]["Title"] = row_tds[1].text_content()
+            maps[row_number]["Author"] = row_tds[2].text_content()
+            maps[row_number]["Size"] = row_tds[3].text_content()
+            maps[row_number]["Width (in)"] = ""        #placeholder to add to in parse_strings()
+            maps[row_number]["Height (in)"] = ""       #placeholder to add to in parse_strings()
+            maps[row_number]["Scale"] = ""             #placeholder to add to in parse_strings()
+            maps[row_number]["Description"] = ""       #placeholder to add to in parse_strings()
+            maps[row_number]["Provenance"] = ""        #placeholder to add to in parse_strings()
+            maps[row_number]["Year"] = row_tds[4].text_content()
+            maps[row_number]["Category"] = row_tds[5].text_content()
 
+        # if only one td, then it is a new line of the "contents" column
         elif len(row_tds) == 1:
+            
+            # extract text from td object, collapsing into one line (without line breaks)
             text = row_tds[0].text_content().strip().replace("\n", "")
 
-            if "Gift" in text:
-                maps[str(row_number)]["Provenance"] = text
-            #TODO: solve the problem when scale is in the full description tr so then there is no description text
+            # sort text into the right dict key by text
+            if " Scale" in text:
+                text, scale = text.split(" Scale")
+                maps[row_number]["Description"] += text + " "
+                maps[row_number]["Scale"] = "Scale" + scale
+            elif "Gift" in text:
+                maps[row_number]["Provenance"] = text
             elif "Scale" in text:
-                maps[str(row_number)]["Scale"] = text
+                maps[row_number]["Scale"] = text
             elif "\"x" in text:
-                maps[str(row_number)]["Size"] += " " + text
+                maps[row_number]["Size"] += " " + text
             else:
-                maps[str(row_number)]["Description"] += text + " "
-
+                maps[row_number]["Description"] += text + " "
 
     # string parsing for additional size columns (width, height)
     for row_dict in maps.values():
-        row_dict["Size"] = row_dict["Size"].replace(":", ": ")
-        row_dict["Size"] = row_dict["Size"].replace("\" x", "\"x")
-        row_dict["Size"] = row_dict["Size"].replace("\"x ", "\"x")
 
-        #edge case - if no x in dimensions, so adding an x in when relevant
-        strs_to_replace = []
-        for integer in range(0, 10):
-            string = "\"" + str(integer)
-            replacement = "\"x" + str(integer)
-            strs_to_replace.append((string,replacement))
-        
-        for string, replacement in strs_to_replace:
-            row_dict["Size"] = row_dict["Size"].replace(string, replacement)
+        #use parse strings function on each row in maps
+        parse_dimensions(row_dict)
 
-        # separate all words in key "Size"
-        for word in row_dict["Size"].split():
-
-            #look at first string that contains "x 
-            if ("\"x" or "\" x " or "\" x") in word:
-                dimensions = word.replace("\"", "").replace("x", " ").replace(",", "").split()
-                print(dimensions)
-                row_dict["Width (in)"] = float(dimensions[0])
-                row_dict["Height (in)"] = float(dimensions[1])
-                break
-
+    # returns full, restructured dictionary
     return maps
 
-def get_csv(root):
-    """
-    """
-    #root = get_root("http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible_files/sheet001.htm")
-    maps_dict = scrape_page(root)
 
-    #convert to dataframe
+def parse_dimensions(row_dict):
+    """
+    Takes a row_dict object created by page_to_dict() and parses the dimension values
+    (width and height) that are saved within the scale key/value pair.
+
+    Inputs: row_dict (dict)
+    Returns: None (changes dict in place)
+    """
+    row_dict["Size"] = row_dict["Size"].replace(":", ": ")
+    row_dict["Size"] = row_dict["Size"].replace("\" x", "\"x")
+    row_dict["Size"] = row_dict["Size"].replace("\"x ", "\"x")
+
+    #edge case - if no x in dimensions, so adding an x in when relevant
+    strs_to_replace = []
+    for integer in range(0, 10):
+        string = "\"" + str(integer)
+        replacement = "\"x" + str(integer)
+        strs_to_replace.append((string,replacement))
+    
+    for string, replacement in strs_to_replace:
+        row_dict["Size"] = row_dict["Size"].replace(string, replacement)
+
+    # separate all words in key "Size"
+    for word in row_dict["Size"].split():
+
+        #look at first string that contains "x 
+        if ("\"x" or "\" x " or "\" x") in word:
+            dimensions = word.replace("\"", "").replace("x", " ").replace(",", "").split()
+            
+            row_dict["Width (in)"] = float(dimensions[0])
+            row_dict["Height (in)"] = float(dimensions[1])
+            break
+    
+    # clean Description to replace two spaces with one.
+    row_dict["Description"] = row_dict["Description"].replace("  ", " ")
+
+
+def get_restructured_data():
+    """
+    """
+    maps_dict = page_to_dict("http://chsmedia.org/media/fa/fa/LIB/CityChicagoMapsAccessible_files/sheet001.htm")
+
+    #convert dictionary to dataframe for analysis
     maps_df = pd.DataFrame.from_dict(maps_dict, orient='index')
 
-    #return maps_df
-
-    # save to csv in filepath folder
-    maps_df.to_excel("output/city_of_chicago_maps.xlsx",
+    # save to csv in restructured_data folder as an excel file
+    maps_df.to_excel("restructured_data/city_of_chicago_maps.xlsx",
                      sheet_name='City of Chicago Maps')
